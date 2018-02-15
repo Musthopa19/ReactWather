2da8c6d5e2f51801068f0f621ea6debbf2a6dada
Herlan Mustopa <herlan.lovecode@gmail.com>
2018-02-15 15:40:59 +0700

    Add Commit\

--- server.js
+++ server.js
@@ -12,6 +12,6 @@ app.use(function(req, res, next) {
 
 app.use(express.static('public'));
 app.listen(PORT, function() {
-    console.log('Server sudah berjalan' + PORT);
+    console.log('Server sudah berjalan ' + PORT);
 
 });
