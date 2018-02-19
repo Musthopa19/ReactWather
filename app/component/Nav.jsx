59aaa37307ce9d489d5cfbfb8c40d47ad657927e
Herlan Mustopa <herlan.lovecode@gmail.com>
2018-02-19 16:52:00 +0700

    Add Foundation

--- app/component/Nav.jsx
+++ app/component/Nav.jsx
@@ -5,16 +5,30 @@ var Nav = React.createClass({
     render :function(){
 
         return(
-          <div>
-            <h2> Nav Component </h2>
-            <IndexLink to="/" activeClassName="active" activeStyle={{fontWeight:'bold'}}>Get Weather</IndexLink>
-            <Link to="/about" activeClassName="active" activeStyle={{fontWeight:'bold'}}>About</Link>
-            <Link to="/examples" activeClassName="active" activeStyle={{fontWeight:'bold'}}>Examples</Link>
+          <div className="top-bar">
+            <div className="top-bar-left">
+              <ul className="menu">
+              <li className="menu-text">React Weather App</li>
+              <li>
+                  <IndexLink to="/" activeClassName="active" activeStyle={{ fontWeight: 'bold'}}>Get Weather</IndexLink>
+              </li>
+              <li>
+                 <Link to="/about"activeClassName="active"activeStyle={{fontWeight: 'bold'}}>About</Link>
+              </li>
+                 <li>
+                 <Link to="/examples"activeClassName="active"activeStyle={{fontWeight: 'bold'}}>Examples</Link>
+              </li>
+              </ul>
 
-            <a href="#/about"> Go to About</a>
+            </div>
+            <div className="top-bar-right">
+
+            </div>
           </div>
+
     );
     }
 });
 
 module.exports=Nav;
+
